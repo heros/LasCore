e06c3d8f53e07487b9f7532bb7fdf8db28000258
lasoto <nansin_@abv.bg>
2013-04-22 22:36:05 +0300

    Change Trinity to Las

--- src/server/shared/Database/QueryResult.h
+++ src/server/shared/Database/QueryResult.h
@@ -57,7 +57,7 @@ class ResultSet
         MYSQL_FIELD* _fields;
 };
 
-typedef Trinity::AutoPtr<ResultSet, ACE_Thread_Mutex> QueryResult;
+typedef Las::AutoPtr<ResultSet, ACE_Thread_Mutex> QueryResult;
 
 class PreparedResultSet
 {
@@ -102,7 +102,7 @@ class PreparedResultSet
 
 };
 
-typedef Trinity::AutoPtr<PreparedResultSet, ACE_Thread_Mutex> PreparedQueryResult;
+typedef Las::AutoPtr<PreparedResultSet, ACE_Thread_Mutex> PreparedQueryResult;
 
 #endif
 
